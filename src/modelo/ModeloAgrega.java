1f2e5c2dee69fcad0b3f28a61f50aeae23705466
USUARIO <USUARIO@192.168.14.220>
2017-12-20 20:15:00 -0300

    TallerN4_producto

---
+++ src/modelo/ModeloAgrega.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import Conexion.Conexion;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+/**
+ *
+ * @author Gabriel
+ */
+public class ModeloAgrega {
+    //private ArrayList<Curso> cursos;
+    private Producto producto;
+    
+    public void agregaProducto(int codigo, int precio, int id_cat, String formato4k, String nombre) throws SQLException{
+        System.out.println("Insertar registro...");
+        Statement dec;
+        dec=Conexion.conexion.createStatement();
+        String query1="insert into A_PELICULA (CODIGO, PRECIO, ID_CAT, FORMATO4K, NOMBRE) VALUES ("+codigo+", '"+precio+"', '"+id_cat+"', '"+formato4k+"', '"+nombre+"')";
+        dec.executeUpdate(query1);
+        
+    }
+    
+    public void agregaCategoria(int id_cat, String descripcion) throws SQLException{
+        System.out.println("Insertar registro...");
+        Statement dec;
+        dec=Conexion.conexion.createStatement();
+        String query1="insert into A_CATEGORIA (ID_CAT, DESCRIPCION) VALUES ("+id_cat+", '"+descripcion+"')";
+        dec.executeUpdate(query1);
+        
+    }
+    
+}
